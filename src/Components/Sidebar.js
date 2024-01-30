c4384e5f14ed790a57bd2cd23c3585e6ff8812d9
Your Name <contactamity8@gmail.com>
2024-01-31 02:49:21 +0530

    created layout of the frontened (sideBar,chatArea)

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./myStyles.css";
+const Sidebar = () => {
+  return <div className="sidebar">Sidebar</div>;
+};
+
+export default Sidebar;
---
+++ src/Components/WorkArea.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./myStyles.css";
+
+const WorkArea = () => {
+  return <div className="work-area">WorkArea</div>;
+};
+
+export default WorkArea;
